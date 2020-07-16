4ea988bfe138cd1223c04fed69f2f1f6c59250ef
杨川 <chuan.yang@aliyun.com>
2020-07-16 15:33:13 +0800

    升级3.04

--- @types/withAny.d.ts
+++ @types/withAny.d.ts
@@ -1,5 +1,6 @@
 /**
- * 这个高阶组件中只要用View把WrappedComp包上， taro-demo-app中的Button就会渲染不出来
+ * 这个高阶组件中只要用View把WrappedComp包上， taro-demo-app中的Button就会渲染不出来，
+ * 不用View包裹， 就没有问题
  * @param WrappedComp
  */
 declare const withAny: (WrappedComp: any) => (props: any) => JSX.Element;
