1b3ca073880b4c4d075d5f4f1aeae6448d2afc30
杨川 <chuan.yang@aliyun.com>
2020-07-16 14:07:53 +0800

    first commit

--- @types/withAny.d.ts
+++ @types/withAny.d.ts
@@ -1,7 +1,6 @@
-import { DynamicAnyObject } from 'cpocar-fe-utils-core';
 /**
  * 这个高阶组件中只要用View把WrappedComp包上， taro-demo-app中的Button就会渲染不出来
  * @param WrappedComp
  */
-declare const withAny: (WrappedComp: any) => (props: DynamicAnyObject) => JSX.Element;
+declare const withAny: (WrappedComp: any) => (props: any) => JSX.Element;
 export default withAny;
--- src/withAny.tsx
+++ src/withAny.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { DynamicAnyObject } from 'cpocar-fe-utils-core'
 import { View } from '@tarojs/components'
 
 /**
- * 这个高阶组件中只要用View把WrappedComp包上， taro-demo-app中的Button就会渲染不出来
+ * 这个高阶组件中只要用View把WrappedComp包上， taro-demo-app中的Button就会渲染不出来，
+ * 不用View包裹， 就没有问题
  * @param WrappedComp
  */
 const withAny = (WrappedComp) => {
-    return (props: DynamicAnyObject) => {
-        return <View ><WrappedComp {...props} /></View>
+    return (props) => {
+        return <View><WrappedComp {...props} /></View>
     }
 }
 
